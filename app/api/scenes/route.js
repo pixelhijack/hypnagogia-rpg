9a482ea2b93e93584ec03ba605f416fd7624e78d
Attila Poth <attilapoth@mac.t.hu>
2025-03-05 22:21:25 +0100

    filter authorized content by @names, return in the response

--- app/api/scenes/route.js
+++ app/api/scenes/route.js
@@ -61,18 +61,20 @@ export async function GET(req) {
     const scenes = sceneMetas.map(scene => {
         const markdown = files.find(file => file.name === `${scene.id}.md`)?.content || '';
         const parsed = marked(markdown);
-        const slicedContent = sliceMarkdownByAtNames(markdown);
-        console.log('============= slicedContent', slicedContent);
+        // [ { names: ['@dm'], content: '...' }, { names: ['@player1'], content: '...' } ]
+        const slicedByNames = sliceMarkdownByAtNames(parsed);
+        const authorizedContent = player.shortName === 'dm' ? slicedByNames : slicedByNames.filter(group => group.names.includes(player.shortName));
+        console.log('============= slicedByNames', player.shortName, authorizedContent);
         return { 
             id: scene.id, 
             title: scene.title, 
             startDate: scene.startDate,
             endDate: scene.endDate,
-            content: parsed.replace(/@\w+/g, ''), 
-            slicedContent 
+            content: authorizedContent.map(group => group.content).join('').replace(/@\w+/g, ''), 
+            slicedByNames 
         };
     });
-    console.log('========= GET /api/scenes', player);
+    console.log('========= GET /api/scenes player', player);
   
     return Response.json({player, scenes});
   }).catch((error) => {
@@ -164,70 +166,3 @@ export async function POST(req) {
   
     return result;
   }
-
-  /*  
-  export async function GET(req) {
-  const session = await getServerSession(authOptions);
-  if (!session) return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
-
-  const players = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/data/players.json"), "utf-8"));
-  const player = players.find(p => p.email === session.user.email);
-  const sceneFiles = fs.readdirSync(path.join(process.cwd(), "app/data/scenes"));
-  const scenes = sceneFiles
-    .map(file => {
-        const scene = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/data/scenes", file), "utf-8"));
-        return scene;
-    })
-    .filter(scene => scene.players.includes(session.user.email));
-  console.log('========= GET /api/scenes', player);
-  
-  return Response.json({player, scenes});
-}
-
-export async function POST(req) {
-    const session = await getServerSession(authOptions);
-    if (!session) return new Response(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
-  
-    try {
-        const body = await req.json(); // Parse the request body
-        const { sceneId, content } = body;
-  
-        // get player character name
-        const players = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/data/players.json"), "utf-8"));
-        const player = players.find(p => p.email === session.user.email);
-        // get scene file which is updated, push a message
-        const sceneFiles = fs.readdirSync(path.join(process.cwd(), "app/data/scenes"));
-        const sceneToUpdate = sceneFiles
-            .map(file => {
-                const scene = JSON.parse(fs.readFileSync(path.join(process.cwd(), "app/data/scenes", file), "utf-8"));
-                return scene;
-            })
-            .find(scene => scene.id === sceneId);
-        
-        const messagesByUser = sceneToUpdate.messages.filter(message => message.character === player.character);
-
-        console.log('========= POST /api/scenes', player, sceneToUpdate);
-
-        if(messagesByUser.length > 3) {
-            return new Response(JSON.stringify({ error: "Too many messages" }), { status: 500 });
-        }
-        sceneToUpdate.messages.push({ 
-            character: player.character,
-            updated: new Date().toISOString(), 
-            content 
-        });
-
-        // save file
-        const sceneFilePath = path.join(process.cwd(), "app/data/scenes", `${sceneId}.json`);
-        fs.writeFileSync(sceneFilePath, JSON.stringify(sceneToUpdate, null, 2));
-
-
-        return new Response(JSON.stringify({ success: true }), { status: 200 });
-    } catch (error) {
-        console.error('Error parsing request body:', error);
-        return new Response(JSON.stringify({ error: "Internal Server Error: api/scenes/route.js" }), { status: 500 });
-    }
-  }
-  
-  
-  */
